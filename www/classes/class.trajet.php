06a1d9a57dd1c683997c3d149337f392be062c34
Ayico <peyre.arthur@outlook.fr>
2024-02-29 15:01:28 +0100

    Ajout des classes Trajet et TrajetDAO

---
+++ www/classes/class.trajet.php
@@ -0,0 +1,33 @@
+<?php
+class Trajet {
+    // Variables
+    private $id;
+    private $id_date;
+    private $id_horaire;
+    private $id_direction;
+
+    // Méthodes et fonctions
+    public function __construct($id, $id_date, $id_horaire, $id_direction) {
+        $this->id = $id;
+        $this->id_date = $id_date;
+        $this->id_horaire = $id_horaire;
+        $this->id_direction = $id_direction;
+    }
+
+    public function getIdTrajet() {
+        return $this->id;
+    }
+
+    public function getIdDate() {
+        return $this->id_date;
+    }
+
+    public function getIdHoraire() {
+        return $this->horaire;
+    }
+
+    public function getIdDirection() {
+        return $this->id_direction;
+    }
+}
+?>
---
+++ www/classes/class.trajetDAO.php
@@ -0,0 +1,49 @@
+<?php
+include_once('./class.trajet.php');
+
+class Trajet {
+    // Variables
+    private $conn;
+
+    // Méthodes et fonctions
+    public function setConnection(PDO $connection) {
+        $this->conn = $connection;
+    }
+
+    public function __construct() {
+        $monPDO=GestionConnexion::getConnexion();
+        $this->setConnection($monPDO);
+    }
+
+    public function loadByID($idTrajet) {
+        // Renvoie l'objet Trajet correspondant à l'identifiant passée en paramètre
+        $sql = "SELECT * FROM trajets WHERE id=:id";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':id', $idTrajet);
+        $stmt->execute();
+
+        $tuple = $stmt->fetch();
+
+        $objTrajet = new Trajet($tuple['id_trajet'], $tuple['id_date'], $tuple['id_horaire'], $tuple['id_direction']);
+        
+        return $objTrajet;
+    }
+
+    public function insertTrajet(Trajet $objTrajet) {
+        // Enregistre dans la base l'objet passé en paramètre
+        $sql = "INSERT INTO trajets VALUES (:id, :id_date, :id_horaire, :id_direction);";
+
+        $stmt = $_db->prepare($sql);
+
+        $stmt->bindParam(':id', $objTrajet->getIdTrajet());
+        $stmt->bindParam(':id_date', $objTrajet->getIdDate());
+        $stmt->bindParam(':id_horaire', $objTrajet->getIdHoraire());
+        $stmt->bindParam(':id_direction', $objTrajet->getIdDirection());
+
+        $bool = ($stmt->execute());
+
+        return $bool;
+    }
+}
+?>
