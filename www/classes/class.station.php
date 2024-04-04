57b55dcc09f324810051667adbb21bb6687e7926
Andernier <122368902+Andernier@users.noreply.github.com>
2024-04-04 12:15:20 +0200

    Update class.station.php

--- www/classes/class.station.php
+++ www/classes/class.station.php
@@ -13,7 +13,26 @@ class Station{
 
     public function getId(){
         return $this->id;
-    }  
-}
+    }
+    
+    public function getVille(){
+        return $this->ville;
+    }
 
-?>
+    public function getLieu(){
+        return $this->lieu;
+    }
+
+    public function setId($lId){
+        $this->id = $lId ;
+    }
+    
+    public function setVille($laVille){
+        $this->ville ;
+    }
+
+    public function setLieu($leLieu){
+        $this->lieu = $leLieu;
+    }
+    
+}
