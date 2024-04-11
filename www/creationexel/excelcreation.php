e1b57ffaecf9087b415d13e1809b8c38519c26d5
Andernier <122368902+Andernier@users.noreply.github.com>
2024-04-11 12:37:59 +0200

    Update excelcreation.php

--- www/creationexel/excelcreation.php
+++ www/creationexel/excelcreation.php
@@ -1,6 +1,7 @@
 <?php
 require_once("./PHP_XLSXWriter-master/xlsxwriter.class.php");
 
+//donnee tempo
 $data = array(
     array('Aller', 'Retour', 'nombre'),
     array('Anglet', 'Pau', '10'),
@@ -9,14 +10,14 @@ $data = array(
 
 $writer = new XLSXWriter();
 $writer->writeSheet($data);
-$writer->writeToFile('php://output'); // Écriture du fichier dans la sortie HTTP
+$writer->writeToFile('php://output'); 
+//ecriture du fichier dans la sortie HTTP
 
-// En-têtes pour forcer le téléchargement
+//Pour le téléchargement
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment;filename="test.xlsx"');
 header('Cache-Control: max-age=0');
 
-// Flusher le tampon de sortie
-flush();
+flush();//Sert à la memoire tampon
 
 ?>
