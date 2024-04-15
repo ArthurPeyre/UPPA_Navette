af3464d7a0703acb3bd177c86b967980b1f450d5
Andernier <122368902+Andernier@users.noreply.github.com>
2024-04-15 16:11:10 +0200

    suppression ancien fichier

--- www/creationexel/excelcreation.php
+++ www/creationexel/excelcreation.php
@@ -1,23 +1,14 @@
 <?php
-require_once("./PHP_XLSXWriter-master/xlsxwriter.class.php");
-
-//donnee tempo
-$data = array(
-    array('Aller', 'Retour', 'nombre'),
-    array('Anglet', 'Pau', '10'),
-    array('Anglet', 'Pau', '3'),
-);
-
-$writer = new XLSXWriter();
-$writer->writeSheet($data);
-$writer->writeToFile('php://output'); 
-//ecriture du fichier dans la sortie HTTP
-
-//Pour le téléchargement
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="test.xlsx"');
-header('Cache-Control: max-age=0');
-
-flush();//Sert à la memoire tampon
-
+        header("Content-type: application/force-download");
+        header("Content-Disposition: attachment; filename=test.xls");
+        echo '<table>'.chr(13);
+        echo '    <tr>'.chr(13);
+        echo '        <td>Cellule 1:1</td>'.chr(13);
+        echo '        <td>Cellule 1:2</td>'.chr(13);
+        echo '    </tr>'.chr(13);
+        echo '    <tr>'.chr(13);
+        echo '        <td>Cellule 2:1</td>'.chr(13);
+        echo '        <td>Cellule 2:2</td>'.chr(13);
+        echo '    </tr>'.chr(13);
+        echo '</table>'.chr(13);
 ?>
