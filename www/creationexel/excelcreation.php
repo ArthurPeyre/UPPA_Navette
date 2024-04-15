6a53edfa3c0599f1dd0edf5f8880a0e5aea4bcd9
Andernier <122368902+Andernier@users.noreply.github.com>
2024-04-15 20:42:11 +0200

    test Excel
    Ce n'est pas la version finale, ici juste un test on récupère pas les données encore depuis la reservation.

--- www/creationexel/excelcreation.php
+++ www/creationexel/excelcreation.php
@@ -1,14 +1,67 @@
 <?php
+include_once('../conn.php');
+
+session_start();
+
+$conn = conn();
+
+if (!isset($_SESSION['Utilisateur']) && $_SESSION['Utilisateur']->getType() < 1) {
+    header('Location: ../index.php');
+}
+
+// Définir le fuseau horaire
+date_default_timezone_set('Europe/Paris');
+setlocale(LC_TIME, 'fr_FR.utf8','fra');
+
+// Traduction des mois en français
+$mois = array(
+    'January' => 'Janvier',
+    'February' => 'Février',
+    'March' => 'Mars',
+    'April' => 'Avril',
+    'May' => 'Mai',
+    'June' => 'Juin',
+    'July' => 'Juillet',
+    'August' => 'Août',
+    'September' => 'Septembre',
+    'October' => 'Octobre',
+    'November' => 'Novembre',
+    'December' => 'Décembre'
+);
+
+
+// Liste des TRAJETS
+$stmt = $conn->prepare("SELECT utilisateurs.nom, utilisateurs.prenom, utilisateurs.email, utilisateurs.phone, CONCAT(depart.ville, ', ', depart.lieu) AS lieuDepart, CONCAT(arrivee.ville, ', ', arrivee.lieu) AS lieuArrivee
+                        FROM utilisateurs 
+                        INNER JOIN reserver ON utilisateurs.id_utilisateur = reserver.id_utilisateur
+                        INNER JOIN lieux AS depart ON reserver.id_lieuDepart = depart.id_lieu
+                        INNER JOIN lieux AS arrivee ON reserver.id_lieuArrivee = arrivee.id_lieu
+                        WHERE reserver.id_trajet = :trajet
+                        ORDER BY reserver.id_lieuDepart, reserver.id_lieuArrivee");
+$date = date('Y-m-01');
+$stmt->bindParam(':trajet', $_POST['idTrajet']);
+$stmt->execute();
+$lstUsers = $stmt->fetch();
+
         header("Content-type: application/force-download");
         header("Content-Disposition: attachment; filename=test.xls");
         echo '<table>'.chr(13);
         echo '    <tr>'.chr(13);
-        echo '        <td>Cellule 1:1</td>'.chr(13);
-        echo '        <td>Cellule 1:2</td>'.chr(13);
+        echo '        <td>Nom Prénom</td>'.chr(13);
+        echo '        <td>Email</td>'.chr(13);
+        echo '        <td>Téléphone</td>'.chr(13);
+        echo '        <td>Lieu Départ</td>'.chr(13);
+        echo '        <td>Lieu Arrivée</td>'.chr(13);
         echo '    </tr>'.chr(13);
+        while ($lstUsers != NULL) {
         echo '    <tr>'.chr(13);
-        echo '        <td>Cellule 2:1</td>'.chr(13);
-        echo '        <td>Cellule 2:2</td>'.chr(13);
+        echo '        <td>'.$lstUsers['nom'].' '.$lstUsers['prenom'].'</td>'.chr(13);
+        echo '        <td>'.$lstUsers['email'].'</td>'.chr(13);
+        echo '        <td>'.$lstUsers['phone'].'</td>'.chr(13);
+        echo '        <td>'.$lstUsers['lieuDepart'].'</td>'.chr(13);
+        echo '        <td>'.$lstUsers['lieuArrivee'].'</td>'.chr(13);
         echo '    </tr>'.chr(13);
+        $lstUsers = $stmt->fetch();
+        }
         echo '</table>'.chr(13);
 ?>
